51bf056945e5926beb1e83d3d36f12a471207016
AlbinMurtezaj <122055604+AlbinMurtezaj@users.noreply.github.com>
2023-05-28 02:05:30 +0200

    update

--- src/main/java/com/example/projektiknk/App.java
+++ src/main/java/com/example/projektiknk/App.java
@@ -13,11 +13,15 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(
+<<<<<<< Updated upstream
                 App.class.getResource("login.fxml")
+=======
+                App.class.getResource("signup.fxml")
+>>>>>>> Stashed changes
         );
 
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Login");
+        stage.setTitle("Home");
         stage.setScene(scene);
         stage.show();
     }
